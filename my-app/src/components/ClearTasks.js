fc33043bbd23905aa16e623050add67fc3299779
John <john@ruffell>
2022-12-24 14:56:13 -0500

    added supabase connection and prompt before delete

--- my-app/src/components/ClearTasks.js
+++ my-app/src/components/ClearTasks.js
@@ -1,9 +1,38 @@
-import { Flex, Button } from '@chakra-ui/react';
+import { Flex, Button, useToast } from '@chakra-ui/react';
+import { useState } from 'react';
+import supabase from '../supabase';
 
 export default function ClearTasks() {
+    const [loading, setLoading] = useState(false);
+    const toast = useToast();
+
+    async function handleClear() {
+        if (!window.confirm('Are you sure you want to clear all tasks?')) return;
+        
+        setLoading(true);
+        const {data, error } = await supabase.from('todos').delete().not('text', 'eq', 'Do not delete me');
+        setLoading(false);
+
+        toast({
+            title: error || 'Tasks cleared',
+            position: 'top',
+            status: error ? 'error' : 'success',
+            duration: 2000,
+            isClosable: true,
+          });
+    }
     return (
         <Flex>
-            <Button colorScheme="gray" px="8" h="45" color="gray.500" mt="10">
+            <Button 
+                colorScheme="gray" 
+                px="8"
+                h="45" 
+                color="gray.500" 
+                mt="10"
+                onClick={handleClear}
+                isLoading={loading}
+                loadingText="Clearing tasks"
+            >
                 Clear Tasks
             </Button>
         </Flex>
