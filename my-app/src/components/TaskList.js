45bfcdb81a315131a4d95aadebe17d192f825433
John <john@ruffell>
2022-12-24 14:56:45 -0500

    added supabase realtime handler

--- my-app/src/components/TaskList.js
+++ my-app/src/components/TaskList.js
@@ -1,16 +1,47 @@
-import { VStack, StackDivider, HStack, Text, Image, Box} from '@chakra-ui/react';
-import DeleteTask from './DeleteTask';
+import { VStack, StackDivider, HStack, Text, Image, Box } from '@chakra-ui/react';
+import DeleteTask from './DeleteTask.js';
 import ClearTasks from './ClearTasks.js';
 import img from '../images/empty.svg';
+import supabase from '../supabase.js';
+import { useEffect, useState } from 'react';
 
 export default function TaskList() {
+    const [tasks, setTasks] = useState([]);
 
-    // If there are no tasks, show placeholder image
-    return (
-        <Box align="center">
-            <Image src={img} mt="30px" maxW="95%" />
-        </Box>
-    );
+    async function fetchData() {
+        let { data: tasks, error } = await supabase.from('todos').select('*');
+
+        setTasks(tasks);
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    useEffect(() => {
+        const todoListener = supabase.channel('custom-all-channel')
+        .on(
+        'postgres_changes',
+        { event: '*', schema: 'public', table: 'todos' },
+        (payload) => {
+            fetchData();
+            //console.log('Change received!', payload)
+        }
+        )
+        .subscribe();
+
+        return () => {
+            todoListener.unsubscribe();
+        };
+    })
+
+    if (!tasks.length) {
+        return (
+            <Box align="center">
+                <Image src={img} mt="30px" maxW="95%" />
+            </Box>
+        );
+    }
 
     return (
         <>
@@ -24,13 +55,14 @@ export default function TaskList() {
                 maxW={{ base: '90vw', sm: '80vw', lg: '50vw', xl: '30vw' }}
                 alignItems="stretch"
             >
-                {/* <HStack key="">
-                    <Text w="100%" p="8px" borderRadius="lg">
-                        Wash the dishes
-                    </Text>
-                    <DeleteTask />
-                </HStack> */}
-
+                {tasks.map(task => (
+                    <HStack key={task.id}>
+                        <Text w="100%" p="8px" borderRadius="lg">
+                            {task.text}
+                        </Text>
+                        <DeleteTask id={task.id}/>
+                    </HStack>
+                ))}
             </VStack>
 
             <ClearTasks />
